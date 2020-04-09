c5263ccab6ff51716e3b4fbf4b19918c19736b84
lucascalero <caleroforero.l@husky.neu.edu>
2020-04-09 13:27:24 -0400

    added serializing to utils

--- 7DegreesOfLinus/src/utils.py
+++ 7DegreesOfLinus/src/utils.py
@@ -39,3 +39,22 @@ def remove_whitespace(data):
 ## Changes a string to a lsit
 def string_to_list(data):
     return eval(data)
+
+
+
+def dataframe_decoder(df):
+    if "schema" in df and "data" in df:
+        sc_dict = df["schema"]
+        sc = Schema(sch=sc_dict["schema_list"],nrows=sc_dict["nrows"],ncols=sc_dict["ncols"])
+        return Dataframe(data=df["data"],sch=sc)
+    return df
+
+
+def serialize_dataframe(df: Dataframe) -> str:
+    df_dict = vars(df)
+    df_dict['schema'] = vars(df.schema)
+    print(df_dict)
+    return json.dumps(df_dict)
+
+def deserialize_dataframe(str_df) -> Dataframe:
+  return json.loads(str_df, object_hook=dataframe_decoder)
